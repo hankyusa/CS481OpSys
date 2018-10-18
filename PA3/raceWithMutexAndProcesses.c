32093096f08b544e56bbe5c2215d315179b22a05
Luke Hanks <hankyusa@gmail.com>
2018-10-17 23:47:06 -0600

    Update

--- PA3/raceWithMutexAndProcesses.c
+++ PA3/raceWithMutexAndProcesses.c
@@ -2,6 +2,7 @@
 /* raceWithMutexAndProcesses.c                             */
 /*=========================================================*/
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
